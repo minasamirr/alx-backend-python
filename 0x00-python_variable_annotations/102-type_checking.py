e30270e8d368ee3d8d6909e608a0a1aa15ae9b04
minasamirr <mina430q@gmail.com>
2024-07-05 14:15:07 +0300

    task

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -24,3 +24,10 @@ def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
         for i in range(factor)
     ]
     return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
