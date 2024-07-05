a127c9a528dccc5cb6c4e7ba758fd18d09a5c8e9
minasamirr <mina430q@gmail.com>
2024-07-05 14:19:13 +0300

    task

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -21,7 +21,7 @@ def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
     """
     zoomed_in: List[int] = [
         item for item in lst
-        for i in range(factor)
+        for _ in range(factor)
     ]
     return zoomed_in
 
