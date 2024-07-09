d49d25ed0b83e8baf41ee9fa1d747b89aacdbc34
minasamirr <mina430q@gmail.com>
2024-07-09 20:54:20 +0300

    task

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-This module contains an asynchronous comprehension that collects random numbers.
+This module contains an asynchronous comprehension that collects random
+numbers.
 """
 from typing import List
 async_generator = __import__('0_async_generator').async_generator
